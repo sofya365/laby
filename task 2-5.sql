0494a1e2201c05c683bd6b43e70ad1a393e618f7
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-10-20 17:53:04 +0400

    Delete task 2-5.sql

--- task 2-5.sql
+++
@@ -1,4 +0,0 @@
-/* Выбрать объекты, которые платны для членов клуба, но стоимость 
-использование не превышает 1/50 от стоимость месячного обслуживания 
-(monthlymaintenance).*/ 
-select facility from facilities where guestcost < (monthlymaintenance/50) or membercost < (monthlymaintenance/50) and membercost!=0;
