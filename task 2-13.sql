d0f94922494fe0379f7ecfe5f2c1a586c50f049b
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-10-20 17:51:52 +0400

    Delete task 2-13.sql

--- task 2-13.sql
+++
@@ -1,3 +0,0 @@
-/* Выберите имя, фамилию и дату вступления в клуб последних из 
-всех вступивших.*/
-select m.firstname, m.surname, b.starttime from members as m join bookings as b on m.memid = b.memid order by b.starttime desc;
