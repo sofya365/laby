a3bd7bda911010fca84be6301c596913a979759f
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-10-20 17:52:11 +0400

    Delete task 2-14.sql

--- task 2-14.sql
+++
@@ -1,4 +0,0 @@
-/* Выберите самый дорогой и самый дешевый объект.*/
-select max(initialoutlay) from facilities;
-select facility from facilities where initialoutlay = (select max(initialoutlay) from facilities)
-or initialoutlay = (select min(initialoutlay) from facilities);
