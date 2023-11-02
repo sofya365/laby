0ebe5650ef862e82da3e1cf09bb5f2a7b26fad57
sofya365 <sofnikolaewa@yandex.ru>
2023-11-02 18:21:13 +0400

    commit 1

--- lab-3/task 3-1.sql
+++ lab-3/task 3-1.sql
@@ -1,6 +1,6 @@
 /*Выберите лист времке (timestamp) покупки времени 
 использования объектов членом клуба 'David Farrell'*/
-USE cd;
+USE cd; 
 SELECT bookings.starttime, bookings.memid, members.surname, members.firstname, members.memid
 FROM members
 JOIN bookings ON (surname = 'Farrel' AND firstname = 'David') AND members.memid = bookings.memid;
