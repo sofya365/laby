fd5a187b176729dfd3397249f8486700711a8238
sofya365 <sofnikolaewa@yandex.ru>
2023-11-07 01:46:02 +0400

    com 6

---
+++ lab-3/task 3-6.sql
@@ -0,0 +1,21 @@
+/*Выберите список бронирований на 14 сентября 2012 г., чтоимость 
+которых более 30. Включите в выходные данные название заведения, имя 
+участника, отформатированное как одиночное столбец и стоимость. 
+Упорядочивайте по убыванию стоимости и не используйте подзапросы.
+Помните, что у гостей разные затраты по сравнению с участниками 
+(указанные затраты указаны за получасовой «интервал»), а идентификатор 
+гостя всегда имеет идентификатор 0. */
+USE cd;
+SELECT * FROM facilities;
+SELECT facilities.facility,
+concat(members.firstname,' ',members.surname) AS name,
+CASE
+WHEN bookings.memid=0 THEN bookings.slots*facilities.guestcost ELSE bookings.slots*facilities.membercost 
+END AS cost;
+FROM bookings
+JOIN facilities ON bookings.facid=facilities.facid
+JOIN members ON bookings.memid=members.memid
+WHERE EXTRACT(DAY FROM starttime) = 14 AND EXTRACT(MONTH FROM starttime) = 10
+AND EXTRACT(YEAR FROM starttime) = 2012 
+AND ((bookings.memid=0 AND bookings.slots*facilities.guestcost>30) OR (bookings.memid!=0 AND bookings.slots*facilities.membercost>30))
+ORDER BY cost DESC;
