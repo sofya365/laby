a357e193e5b2966a6b136d728f429df40780c542
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-11-17 19:55:13 +0400

    new 6

--- lab-4/task 4-6.sql
+++ lab-4/task 4-6.sql
@@ -1,5 +1,5 @@
 /*Увеличьте стоимость аренды  корта 1 (facid = 0) на 10% для 
 членов и гостей.*/
 USE cd;
-UPDATE facilities SET membercost=membercost*0.1, guestcost=guestcost*0.1 WHERE facid = 0;
+UPDATE facilities SET membercost=membercost*1.1, guestcost=guestcost*1.1 WHERE facid = 0;
 SELECT * FROM facilities;
