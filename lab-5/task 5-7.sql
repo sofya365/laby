dedab80803d59d7a956c6c5b2dc603a532722c1b
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-11-20 12:46:14 +0400

    7

--- lab-5/task 5-7.sql
+++ lab-5/task 5-7.sql
@@ -1,5 +1,5 @@
 /*Найдите общее количество участников (члены + гости), 
 совершивших хотя бы одно бронирование.*/
 USE cd;
-SELECT COUNT(DISTINCT memid) AS 'участники совершившие хотя бы одно брон' FROM bookings
+SELECT COUNT(DISTINCT memid) AS 'участники совершившие хотя бы одно бронирование' FROM bookings
 WHERE memid != 0;
