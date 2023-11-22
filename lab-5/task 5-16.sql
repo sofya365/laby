a251dcdecb775d97d900fbc9475cff25e24aa909
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-11-22 21:22:57 +0400

    new

--- lab-5/task 5-16.sql
+++ lab-5/task 5-16.sql
@@ -1,11 +1,11 @@
 /*Составьте список трех крупнейших объектов, приносящих доход (включая связи). 
 Вывод названия и ранга объекта, отсортированный по рангу и названию объекта.*/
 USE cd;
-SELECT f.facility AS 'объект', RANK() OVER (ORDER BY SUM(CASE 
+SELECT f.facility, RANK() OVER (ORDER BY SUM(CASE 
 WHEN b.memid = 0 THEN f.guestcost * b.slots ELSE f.membercost * b.slots 
 END) DESC) as 'ранг'
 FROM facilities f
 JOIN bookings b ON f.facid = b.facid 
 GROUP BY f.facility 
-ORDER BY ранг
+ORDER BY ранг, f.facility
 LIMIT 3;
