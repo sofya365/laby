7e99613d4ec29b6448eb9e8ea44576ffc7e05c57
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-01 19:13:29 +0400

    new

--- lab-6/task 6-3.sql
+++ lab-6/task 6-3.sql
@@ -1,10 +1,14 @@
 /*Выберите процент использования объектов по месяцам, упорядочив по возрастанию*/
 USE cd;
-SELECT 
-    MONTH(b.starttime) AS months, 
-    f.facility,
-    (SUM(b.slots) / (COUNT(DISTINCT b.facid) * 720 * COUNT(DISTINCT YEAR(b.starttime)))) * 100 AS usage_percent 
-FROM bookings b
-INNER JOIN facilities f ON b.facid = f.facid 
-GROUP BY months, f.facility
-ORDER BY usage_percent ASC;
+WITH slots as (
+  SELECT facility, SUM(b.slots) as rent_count,
+      DATE_FORMAT(b.starttime, "%m %Y") as дата
+    FROM facilities as f
+      JOIN bookings as b ON b.facid = f.facid
+    GROUP BY f.facid, дата
+)
+SELECT s1.facility, CONCAT(ROUND(s1.rent_count / SUM(s2.rent_count) * 100, 1), '%') as usability, s1.дата
+FROM slots as s1
+JOIN slots as s2 ON s1.дата = s2.дата
+GROUP BY s1.facility, s1.rent_count, s1.дата
+ORDER BY CAST(usability as FLOAT) ASC;
