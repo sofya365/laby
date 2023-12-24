eb3f12aae7f7778f5a84965fc87d4111dbde72d5
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-24 18:42:28 +0400

    new

---
+++ lab-7/task 7-1.sql
@@ -0,0 +1,19 @@
+/*Создайте функцию, которая рассчитывает стоимость каждой аренды (для каждой записи таблицы bookings)*/
+USE cd1;
+DELIMITER $$
+DROP FUNCTION IF EXISTS getRentCost $$
+CREATE FUNCTION getRentCost(memid INT, facid INT, slots INT) RETURNS DECIMAL(10, 2)
+    READS SQL DATA
+    NOT DETERMINISTIC
+BEGIN
+    DECLARE income DECIMAL(10, 2);
+    SET income = (
+        SELECT IF(memid = 0, guestcost, membercost) * slots
+        FROM facilities
+        WHERE facid = facilities.facid
+    );
+    RETURN income;
+END $$
+DELIMITER ;
+SELECT getRentCost(memid, facid, slots)
+FROM bookings;
