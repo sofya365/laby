6f39583e130bb2037b7f8da6404f1251c0a0f6b0
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-25 11:41:49 +0400

    view

--- lab-8/task 8-1.sql
+++ lab-8/task 8-1.sql
@@ -97,19 +97,12 @@ AUTO_INCREMENT = 12
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
--- -----------------------------------------------------
--- Table `tm`.`remains`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tm`.`remains` (
-  `treatment` INT NULL DEFAULT NULL,
-  `total_ordered` INT NULL DEFAULT NULL,
-  `available_for_order` INT NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
 USE `tm` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `tm`.`cost_order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`cost_order` (`Имя заказчика` INT, `Заказ` INT, `(amount*cost)` INT);
 
 -- -----------------------------------------------------
 -- Placeholder table for view `tm`.`country_producer`
@@ -122,43 +115,25 @@ CREATE TABLE IF NOT EXISTS `tm`.`country_producer` (`producername` INT, `country
 CREATE TABLE IF NOT EXISTS `tm`.`remains` (`treatment` INT, `total_ordered` INT, `available_for_order` INT);
 
 -- -----------------------------------------------------
--- Placeholder table for view `tm`.`cost_order`
+-- View `tm`.`cost_order`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tm`.`cost_order` (`"Имя заказчика"` INT, `'Заказ'` INT, `'Стоимость заказа '` INT); 
+DROP TABLE IF EXISTS `tm`.`cost_order`;
+USE `tm`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tm`.`cost_order` AS select distinct `ord`.`customername` AS `Имя заказчика`,concat(`tr`.`treatment`,', Количество:',`ord`.`amount`) AS `Заказ`,(`ord`.`amount` * `tr`.`cost`) AS `(amount*cost)` from (`tm`.`orders` `ord` join `tm`.`treatments` `tr` on((`ord`.`treatid` = `tr`.`treatid`)));
 
 -- -----------------------------------------------------
 -- View `tm`.`country_producer`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `tm`.`country_producer`;
 USE `tm`;
-CREATE  OR REPLACE VIEW `country_producer` AS
-	SELECT DISTINCT producername, country FROM producer;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tm`.`country_producer` AS select distinct `tm`.`producer`.`producername` AS `producername`,`tm`.`producer`.`country` AS `country` from `tm`.`producer`;
 
 -- -----------------------------------------------------
 -- View `tm`.`remains`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `tm`.`remains`;
 USE `tm`;
-CREATE  OR REPLACE VIEW `remains` AS
-	SELECT t.treatment,
-SUM(o.amount) AS total_ordered,
-	CASE
-WHEN t.remains - SUM(o.amount) < 0
- THEN 'недоступно для заказа'
-ELSE t.remains - SUM(o.amount)
-END AS available_for_order
-FROM orders o
-INNER JOIN treatments t ON o.treatid = t.treatid
-GROUP BY o.treatid;
-
--- -----------------------------------------------------
--- View `tm`.`cost_order`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `tm`.`cost_order`;
-USE `tm`;
-CREATE  OR REPLACE VIEW `cost_order` AS
-	SELECT DISTINCT customername "Имя заказчика",CONCAT(treatment, ', Количество:', amount) AS 'Заказ', (amount*cost) FROM orders ord
-INNER JOIN treatments tr ON ord.treatid = tr.treatid;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `tm`.`remains` AS select `t`.`treatment` AS `treatment`,sum(`o`.`amount`) AS `total_ordered`,(case when ((`t`.`remains` - sum(`o`.`amount`)) < 0) then 'недоступно для заказа' else (`t`.`remains` - sum(`o`.`amount`)) end) AS `available_for_order` from (`tm`.`orders` `o` join `tm`.`treatments` `t` on((`o`.`treatid` = `t`.`treatid`))) group by `o`.`treatid`;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
