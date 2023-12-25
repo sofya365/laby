8d4b8726a07fe10e4d3edc6d1bccb943ae2ec9ac
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-25 19:05:28 +0400

    new

--- lab-8/apteka.sql
+++ lab-8/apteka.sql
@@ -1,18 +1,136 @@
-/*Заполнение данными*/
+-- MySQL Workbench Forward Engineering
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema tm
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema tm
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `tm` ;
+
+-- -----------------------------------------------------
+-- Table `tm`.`producer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`producer` (
+  `prodid` INT NOT NULL AUTO_INCREMENT,
+  `producername` VARCHAR(100) NOT NULL,
+  `country` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`prodid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 10
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `tm`.`treatments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`treatments` (
+  `treatid` INT NOT NULL AUTO_INCREMENT,
+  `treatment` VARCHAR(100) NOT NULL,
+  `descriptionoftreatment` VARCHAR(500) NULL DEFAULT NULL,
+  `dose` INT NOT NULL,
+  `amountinpackage` INT NOT NULL,
+  `cost` INT NOT NULL,
+  `link` VARCHAR(500) NULL DEFAULT NULL,
+  `receiptdate` TIMESTAMP NOT NULL,
+  `amountofpackage` INT NOT NULL,
+  `prodid` INT NOT NULL,
+  `remains` INT NOT NULL,
+  PRIMARY KEY (`treatid`),
+  INDEX `fk_treatments_prodid` (`prodid` ASC) VISIBLE,
+  CONSTRAINT `fk_treatments_prodid`
+    FOREIGN KEY (`prodid`)
+    REFERENCES `tm`.`producer` (`prodid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 10
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `tm`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`orders` (
+  `orderid` INT NOT NULL AUTO_INCREMENT,
+  `numoforder` INT NOT NULL,
+  `treatid` INT NOT NULL,
+  `amount` INT NOT NULL,
+  `customername` VARCHAR(200) NOT NULL,
+  `phone` VARCHAR(20) NOT NULL,
+  `dateoforder` TIMESTAMP NOT NULL,
+  `dateofdelivery` TIMESTAMP NOT NULL,
+  `dateofbuying` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`orderid`),
+  INDEX `fk_orders_treatid` (`treatid` ASC) VISIBLE,
+  CONSTRAINT `fk_orders_treatid`
+    FOREIGN KEY (`treatid`)
+    REFERENCES `tm`.`treatments` (`treatid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+USE `tm` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `tm`.`remains`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`remains` (`treatment` INT, `available_for_order` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `tm`.`cost_order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`cost_order` (`orderid` INT, `"Имя заказчика"` INT, `'Заказ'` INT, `'Стоимость заказа '` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `tm`.`country_producer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tm`.`country_producer` (`producername` INT, `country` INT);
+
+-- -----------------------------------------------------
+-- View `tm`.`remains`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tm`.`remains`;
 USE `tm`;
+CREATE  OR REPLACE VIEW `remains` AS
+SELECT t.treatment,
+	CASE
+WHEN t.remains - SUM(o.amount) < 0
+ THEN 'недоступно для заказа'
+ELSE t.remains - SUM(o.amount)
+END AS available_for_order
+FROM orders o
+INNER JOIN treatments t ON o.treatid = t.treatid
+WHERE CURRENT_TIMESTAMP() <= o.dateofdelivery
+GROUP BY o.treatid,o.amount, o.dateofdelivery ;
 
-INSERT producer(producername, country) VALUES
-('Bionorica', 'Germany'),
-('Italfarmaco', 'Italy'),
-('USBFarma', 'Belgium');
+-- -----------------------------------------------------
+-- View `tm`.`cost_order`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tm`.`cost_order`;
+USE `tm`;
+CREATE  OR REPLACE VIEW `cost_order` AS
+SELECT orderid, customername AS "Имя заказчика",CONCAT(treatment, ', Количество:', amount) AS 'Заказ', (amount*cost) AS 'Стоимость заказа' FROM orders ord
+INNER JOIN treatments tr ON ord.treatid = tr.treatid;
 
-INSERT treatments(treatment, descriptionoftreatment, dose, amountinpackage, cost, link, receiptdate, amountofpackage, prodid, remains) VALUES
-('Kanefron', 'Комбинированный препарат растительного происхождения', 6, 60, 700, 'https://canephron.ru/', '2023-12-17 10:34:49', 20, 1, 10),
-('Ferlatym', 'Лечение клинически выраженного дефицита железа (железодефицитной анемии)', 2, 20, 1100, 'ccskrf', '2023-10-03 09:13:03', 6, 2, 2),
-('Ataracs', 'Тревожные расстройства при невротических и неврозоподобных состояниях', 25, 25, 112, 'https://www.rlsnet.ru/drugs/ataraks-363','2023-08-29 12:57:00', 15, 3, 5);
+-- -----------------------------------------------------
+-- View `tm`.`country_producer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tm`.`country_producer`;
+USE `tm`;
+CREATE  OR REPLACE VIEW `country_producer` AS
+SELECT DISTINCT producername, country FROM producer;
 
-INSERT orders(numoforder, treatid, amount, customername, phone, dateoforder, dateofdelivery, dateofbuying) VALUES
-(673, 1, 2, 'Mark', 89282157062,'2023-09-30 20:45:34', '2023-10-01 14:30:28', '2023-10-01 14:31:20'),
-(348, 2, 1, 'Alex', 89376559361, '2023-10-05 11:32:48', '2023-10-07 18:00:00', '2023-10-07 18:00:57'),
-(740, 3, 2, 'John', 89279527947, '2023-11-17 15:16:18', '2023-11-23 16:23:56', '2023-11-23 16:25:01');
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
